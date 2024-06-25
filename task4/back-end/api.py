63140e99f88b1e95ccf6b51936825265f5f7ae85
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-24 19:46:09 -0500

    task4 created

---
+++ task4/back-end/api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+File uses Flask to create one
+endpoint that returns “Hello, World!” when called
+"""
+
+
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/api/hello')
+def hello_world():
+    return 'Hello, World!'
+
+# Hosting this Flask app on 0.0.0.0 instead of 127.0.0.1 means
+# that it is reachable outside of the current machine
+# (the current machine being a Docker container which is
+# running inside of your laptop/desktop)
+# Host this Flask app on port 5252
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5252)
